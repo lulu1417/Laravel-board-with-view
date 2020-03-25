d162a15d1ead5cf9648d35249885eed45130b1da
lulu1417 <boa1417@gmail.com>
2020-03-25 18:40:29 +0800

    cors header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,8 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-//header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: *");
+//header("Access-Control-Allow-Methods: *");
+//header("Access-Control-Allow-Headers: *");
 //header("Access-Control-Allow-Headers: Origin, Methods, Accept, Content-Type, Authorization");
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
