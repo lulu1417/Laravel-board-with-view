81a8cdb70ab0d03db9ddef000233a892c74a4949
lulu1417 <boa1417@gmail.com>
2020-03-25 16:45:05 +0800

    debug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@
 |
 */
 //header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
 //header("Access-Control-Allow-Headers: Origin, Methods, Accept, Content-Type, Authorization");
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
