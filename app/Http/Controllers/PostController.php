1a1e9d70b64d9220e5866cc9d341cf06988b3967
lulu1417 <boa1417@gmail.com>
2020-03-25 16:21:20 +0800

    debug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,6 +27,7 @@ class PostController extends Controller
             ->get();
 
         foreach ($posts as $post){
+            ;
 //            $last = $this->transfer($post->created_at);
 //            dd($last);
         }
@@ -76,22 +77,22 @@ class PostController extends Controller
 
     static function transfer($time)
     {
-        if(strpos($time, date("Y-m-d") == 0)){
-            $now = date("Y-m-d H:i:s");
-//            var_dump(strtotime($now));
-            $startdate="2011-3-15 11:50:00";
-//            var_dump(strtotime("2015-11-18 23:00:00"));
-            die();
-            $last = strtotime($now) - strtotime($time);
-             $last['second'] = (strtotime($now) - strtotime($time)); //計算相差之秒數
-            $last['min'] =  (strtotime($now) - strtotime($time))/ (60); //計算相差之分鐘數
-            $last['hour'] =  (strtotime($now) - strtotime($time))/ (60*60); //計算相差之小時數
-
-            return $last;
-
-        }else{
-            return $time;
-        }
-
+//        if(strpos($time, date("Y-m-d") == 0)){
+//            $now = date("Y-m-d H:i:s");
+////            var_dump(strtotime($now));
+//            $startdate="2011-3-15 11:50:00";
+////            var_dump(strtotime("2015-11-18 23:00:00"));
+//            die();
+//            $last = strtotime($now) - strtotime($time);
+//             $last['second'] = (strtotime($now) - strtotime($time)); //計算相差之秒數
+//            $last['min'] =  (strtotime($now) - strtotime($time))/ (60); //計算相差之分鐘數
+//            $last['hour'] =  (strtotime($now) - strtotime($time))/ (60*60); //計算相差之小時數
+//
+//            return $last;
+//
+//        }else{
+//            return $time;
+//        }
+//
     }
 }
